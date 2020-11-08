3fe8d87a220a5025901e39ea36815d933d3e674d
Gurcan Hamali <gurcanhamali@gmail.com>
2020-11-08 18:12:56 +1100

    88. AuthReducer Return Values

--- server/client/src/reducers/authReducer.js
+++ server/client/src/reducers/authReducer.js
@@ -1,7 +1,11 @@
 /* eslint-disable import/no-anonymous-default-export */
+import { FETCH_USER } from '../actions/types';
+
 export default function (state = {}, action) {
   console.log(action);
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false;
     default:
       return state;
   }
