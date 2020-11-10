bce38204c5d9dfcf82c17c9d5b2fb6bbfc98d80c
Gurcan Hamali <gurcanhamali@gmail.com>
2020-11-10 11:25:40 +1100

    158. Custom Field Components

---
+++ server/client/src/components/surveys/SurveyField.js
@@ -0,0 +1,11 @@
+// SurveyField contains logic to render a single label and text input
+import React from 'react';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+  return (
+    <div>
+      <input />
+    </div>
+  );
+};
